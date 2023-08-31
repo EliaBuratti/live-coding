db3977402e146826a12d32c711765a29a0ab9584
EliaBuratti <elia.buratti.dev@gmail.com>
2023-08-31 12:22:23 +0200

    input number with function

--- javascript_es/function/assets/js/main.js
+++ javascript_es/function/assets/js/main.js
@@ -1,1 +1,44 @@
-console.log('ciao mondo');
+// const userNumber = 8;
+
+/* if (userNumber % 2 === 0) {
+    console.log('Il numero è pari');
+} else {
+    console.log('Il numero è dispari');
+}; */
+
+function isEven (userNumber) {
+
+    if (userNumber % 2 === 0) {
+        return true;
+
+    } // si potrebbe usare l'else per fare return false, ma non serve perchè comunque non entra nell'if
+    return false
+};
+
+/* const userNumber = prompt('scegli un numero')
+const result = isEven(userNumber);
+console.log(result);
+
+if (result) {
+    console.log('Il numero è pari');
+
+} else {
+    console.log('Il numero è dispari');
+
+} */
+
+const checkButton = document.querySelector('button');
+const inputNumber = document.getElementById('user_number');
+
+checkButton.addEventListener('click', function() {
+    console.log('ho cliccato il pusante');
+    console.log(`il numero inserito è: ${inputNumber.value}`);
+
+    if (isEven(inputNumber.value)) {
+        console.log('Il numero è pari');
+    
+    } else {
+        console.log('Il numero è dispari');
+    
+    }
+});
