d2aa9365c8ea22bf693c159f894fe2e62b1571a6
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-20 12:34:14 +0200

    add two call axios
    added two call axios and find number higher

--- api/request/main.js
+++ api/request/main.js
@@ -1,4 +1,4 @@
-
+/* 
 const { createApp } = Vue
 
 createApp({
@@ -19,5 +19,46 @@ createApp({
 
             this.randomNumber = response.data.response;
         })
+    }
+}).mount('#app') */
+
+
+/* 
+Tramite due chiamate axios, generiamo 2 numeri casuali: uno per l'utente e uno per il pc. 
+Stampiamo in pagina i due numeri e il vincitore, ossia chi ha il numero più grande. */
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+
+    return {
+
+        playerNumber : '',
+        pcNumber : '' ,
+        
+        }
+    },
+    mounted() {
+        console.log('montato');
+        axios.get('https://flynn.boolean.careers/exercises/api/random/int') //inserisci api https
+        
+        .then(response => {
+            console.log(response);
+
+            this.playerNumber = response.data.response;
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/int') //inserisci api https
+        
+        .then(response => {
+            console.log(response);
+
+            this.pcNumber = response.data.response;
+        })
+
+        //se devo scrivere tante volte, posso usare un for
+        
     }
 }).mount('#app')
