57440d059bd53fb694676c497b073c9f333dfa7d
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-22 11:44:14 +0200

    add scss and bs to project

---
+++ vite_vue/vite_bs/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>ciao mondo</h1>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<!-- <style lang="scss">  scoped limita le classi alla singola app   lang="scss" serve per far riconoscere al browser che è scritto in sass  
+ @use './assets/scss/app.scss' posso importare il scss in questo modo
+</style> -->
+
+<style></style>
+
+
+
+
+<!-- 
+  per installare sass devi scrivere nel terminale questo
+
+  npm add -D sass
+ -->
